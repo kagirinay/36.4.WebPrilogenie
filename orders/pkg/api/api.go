5fae4cbf9511702b9a33f88b962ed0c29b095cb8
kagirinay <kagirinay@yandex.ru>
2023-11-29 19:23:54 +0700

    0.3

--- orders/pkg/api/api.go
+++ orders/pkg/api/api.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 	"orders/pkg/db"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -11,10 +12,10 @@ import (
 // API приложения.
 type API struct {
 	r  *mux.Router // Маршрутизатор запросов
-	db *db.DB
+	db *db.DB      // база данных
 }
 
-// Конструктор API
+// Конструктор API.
 func New(db *db.DB) *API {
 	api := API{}
 	api.db = db
@@ -31,15 +32,65 @@ func (api *API) Router() *mux.Router {
 // Регистрация методов API в маршрутизаторе запросов.
 func (api *API) endpoints() {
 	api.r.HandleFunc("/orders", api.ordersHandler).Methods(http.MethodGet)
-	api.r.HandleFunc("/orders", api.newOrdersHandler).Methods(http.MethodPost)
-	api.r.HandleFunc("/orders/{id}", api.updateOrdersHandler).Methods(http.MethodPatch)
-	api.r.HandleFunc("/orders/{id}", api.deleteOrdersHandler).Methods(http.MethodDelete)
+	api.r.HandleFunc("/orders", api.newOrderHandler).Methods(http.MethodPost)
+	api.r.HandleFunc("/orders/{id}", api.updateOrderHandler).Methods(http.MethodPatch)
+	api.r.HandleFunc("/orders/{id}", api.deleteOrderHandler).Methods(http.MethodDelete)
 }
 
-// ordersHandler создаёт новый заказ
-func (api *API) ordersHandler(w http.ResponseWriter, r http.Request) {
+// ordersHandler возвращает все заказы.
+func (api *API) ordersHandler(w http.ResponseWriter, r *http.Request) {
 	// Получение данных из БД.
 	orders := api.db.Orders()
-	// Отправка данных клиенту в формате JSON
+	// Отправка данных клиенту в формате JSON.
 	json.NewEncoder(w).Encode(orders)
+}
+
+// newOrderHandler создает новый заказ.
+func (api *API) newOrderHandler(w http.ResponseWriter, r *http.Request) {
+	var o db.Order
+	err := json.NewDecoder(r.Body).Decode(&o)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	id := api.db.NewOrder(o)
+	w.Write([]byte(strconv.Itoa(id)))
+}
+
+// updateOrderHandler обновляет данные заказа по ID.
+func (api *API) updateOrderHandler(w http.ResponseWriter, r *http.Request) {
+	// Считывание параметра {id} из пути запроса.
+	// Например, /orders/45.
+	s := mux.Vars(r)["id"]
+	id, err := strconv.Atoi(s)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	// Декодирование в переменную тела запроса,
+	// которое должно содержать JSON-представление
+	// обновляемого объекта.
+	var o db.Order
+	err = json.NewDecoder(r.Body).Decode(&o)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	o.ID = id
+	// Обновление данных в БД.
+	api.db.UpdateOrder(o)
+	// Отправка клиенту статуса успешного выполнения запроса
+	w.WriteHeader(http.StatusOK)
+}
+
+// deleteOrderHandler удаляет заказ по ID.
+func (api *API) deleteOrderHandler(w http.ResponseWriter, r *http.Request) {
+	s := mux.Vars(r)["id"]
+	id, err := strconv.Atoi(s)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	api.db.DeleteOrder(id)
+	w.WriteHeader(http.StatusOK)
 }
